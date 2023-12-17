876e25f25fb4adba7876cc226cf2ed22654b817a
Wanderson Brito <wanderson.brito@gmail.com>
2023-12-17 19:08:27 -0300

    Curso e mentoria criados

---
+++ desafio-dio-poo/src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Kotlin");
+        curso2.setDescricao("Descrição curso Kotlin");
+        curso2.setCargaHoraria(5);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Descrição da mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
